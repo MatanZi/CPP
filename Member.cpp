b986f1da7a164acc004324603b0a0d6621c877be
Matan Zilka <33133645+MatanZi@users.noreply.github.com>
2018-05-23 16:43:53 +0300

    Update Member.cpp

--- Member.cpp
+++ Member.cpp
@@ -4,15 +4,21 @@
 
 #include "Member.h"
 
-int Member::_MemberID=1;
-int Member::ActiveMember =0;
+//---------------------------------------global parameters------------------------------
+int Member::_MemberID=1; // uniqe id for each member
+int Member::ActiveMember =0; // indicates the active memebrs
 
+
+//--------------------------------------- Constractor / Destractur----------------------
+//constractor
+//set member id and incremenet param Activememebr 
 Member::Member() {
     this->MemberID=_MemberID;
     _MemberID++;
     ActiveMember++;
 }
-
+//destractor 
+//earse all memroy of this memebr (also within the folloing map container and the followers map container
 Member::~Member() {
 	map <int, Member*> :: iterator i;
 	for(i=Following.begin(); i!=Following.end();i++)
@@ -26,11 +32,18 @@ Member::~Member() {
 	}
     ActiveMember--;
 }
-
+//----------------------------- Functions -----------------------------------------------------
+//return the size of the followers container
 int Member::numFollowers() {return Followers.size();}
+//return the size of the following container
 int Member::numFollowing() {return Following.size();}
+//return the size of the Active members
 int Member::count() {return ActiveMember;}
 
+
+//this function check if the new member isnt himself and isnt already in the Following map container
+//if so the new memeber is enterd the Folloing container
+//otherwise do nothing
 void Member::follow(Member &toAdd) {
 
     if(&toAdd!= this){
@@ -41,6 +54,10 @@ void Member::follow(Member &toAdd) {
     }
 }
 
+
+//this function check if the new member isnt himself and isnt already in the Folloers map container
+//if so the new memeber is enterd the Followers container
+//otherwise do nothing
 void Member::unfollow(Member &toRemove) {
     if(&toRemove!= this){
         if(Following.count(toRemove.MemberID) == 1) {
